b04c1be0cc2422324367187b38dc1be0c605d56b
ClaudiuBodea <claudiu.bodea99@e-uvt.ro>
2021-03-20 11:53:44 +0200

    Aprob

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Elo World")
+import xml.etree.ElementTree as ET
+
+
+
+class Reader():
+    def read_file(self,file):
+
+        tree = ET.parse(file)
+        root=tree.getroot()
+        return root
+
+
+
+
+
+
+reader=Reader()
+test1=reader.read_file("test1.xml")
+for child in test1.iter('namePrecondition'):
+    print(child.attrib)
+    #print(child.tag)
+
+
+#print(test1)
+
