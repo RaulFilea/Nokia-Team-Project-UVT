2e6ac6ef5c88f0bb0f62d79bb3f7895734c796a5
Raul Filea <raul.filea00@e-uvt.ro>
2021-04-14 13:43:07 +0300

    Reading and storing data into nodes.

---
+++ src/Main.java
@@ -0,0 +1,136 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String [] args) {
+        try {
+            File file = new File("D:\\Scoala\\Y2S2\\TP\\nokiaTP\\huge-test.xml");
+            Scanner myReader = new Scanner(file);
+            Node node;
+            String name = null, link = null;
+            List<String> Prec, Postc, Trig, Ref, Desc;
+            String [] aux;
+            String [] aux2;
+
+            Prec = new ArrayList<>();
+            Postc = new ArrayList<>();
+            Trig = new ArrayList<>();
+            Ref = new ArrayList<>();
+            Desc = new ArrayList<>();
+
+            List<Node> nodes = new ArrayList<>();
+
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+
+                if (line.contains("</node>")) {
+
+                    node = new Node(name, link, Prec, Postc, Trig, Desc, Ref);
+                    nodes.add(node);
+
+                    Prec = new ArrayList<>();
+                    Postc = new ArrayList<>();
+                    Trig = new ArrayList<>();
+                    Ref = new ArrayList<>();
+                    Desc = new ArrayList<>();
+                    name = null;
+                    link = null;
+                }
+
+                if (line.contains("<name>")) {
+                    aux = line.split(">");
+                    aux2 = aux[1].split("<");
+                    name = aux2[0];
+                }
+
+                if (line.contains("<link>")) {
+                    aux = line.split(">");
+                    aux2 = aux[1].split("<");
+                    link = aux2[0];
+                }
+
+                if (line.contains("<nodeListPrecondition>")) {
+                    line = myReader.nextLine();
+                    if (!line.contains("<namePrecondition>")) {
+                        //Prec.add(preaux);
+                    } else {
+                        while (!line.contains("</nodeListPrecondition>")) {
+                            aux = line.split(">");
+                            aux2 = aux[1].split("<");
+                            Prec.add(aux2[0]);
+                            line = myReader.nextLine();
+                        }
+                    }
+                }
+
+                if (line.contains("<nodeListPostCondition>")) {
+                    line = myReader.nextLine();
+                    if (!line.contains("<namePostCondition>")) {
+                        //Postc.add(postaux);
+                    } else {
+                        while (!line.contains("</nodeListPostCondition>")) {
+                            aux = line.split(">");
+                            aux2 = aux[1].split("<");
+                            Postc.add(aux2[0]);
+                            line = myReader.nextLine();
+                        }
+                    }
+                }
+
+                if (line.contains("<nodeListTrigger>")) {
+                    line = myReader.nextLine();
+                    if (!line.contains("<nameTrigger>")) {
+                        //Trig.add(trigaux);
+                    } else {
+                        while (!line.contains("</nodeListTrigger>")) {
+                            aux = line.split(">");
+                            aux2 = aux[1].split("<");
+                            Trig.add(aux2[0]);
+                            line = myReader.nextLine();
+                        }
+                    }
+                }
+
+                if (line.contains("<nodeListDescription>")) {
+                    line = myReader.nextLine();
+                    if (!line.contains("<nameDescription>")) {
+                        //Desc.add(descaux);
+                    } else {
+                        while (!line.contains("</nodeListDescription>")) {
+                            aux = line.split(">");
+                            aux2 = aux[1].split("<");
+                            Desc.add(aux2[0]);
+                            line = myReader.nextLine();
+                        }
+                    }
+                }
+
+                if (line.contains("<nodeListReferences>")) {
+                    line = myReader.nextLine();
+                    if (!line.contains("<nameReferences>")) {
+                        //Ref.add(refaux);
+                    } else {
+                        while (!line.contains("</nodeListReferences>")) {
+                            aux = line.split(">");
+                            aux2 = aux[1].split("<");
+                            Ref.add(aux2[0]);
+                            line = myReader.nextLine();
+                        }
+                    }
+                }
+            }
+            myReader.close();
+
+            for (Node el : nodes) {
+                System.out.println(el);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
